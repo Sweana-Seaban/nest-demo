e5c619dfa32ad42aab0d8a8f71ce18f04fe1e9b4
sweana <sweana@bititude.com>
2024-02-09 16:21:53 +0530

    Add empty CRUD endpoints

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  @Get()
+  getBooks() {
+    return [];
+  }
+
+  @Get(':id')
+  getOneBook() {
+    return {};
+  }
+
+  @Post()
+  createBook() {
+    return {};
+  }
+
+  @Put(':id')
+  updateBook() {
+    return {};
+  }
+
+  @Delete(':id')
+  removeBook() {
+    return {};
+  }
+}
